42175bab1d6b4512a2be4c885f33bc27860faa72
HotsykVictor <vitia2372087@gmail.com>
2023-09-19 15:36:32 +0300

    rename fonts folder

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -5,37 +5,37 @@ import localFont from 'next/font/local'
 const font = localFont({
   src: [
     {
-      path: '../public/static/font/GTWalsheimPro-Light.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/static/font/GTWalsheimPro-Regular.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/static/font/GTWalsheimPro-Thin.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../public/static/font/GTWalsheimPro-UltraBold.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-UltraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../public/static/font/GTWalsheimPro-Bold.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-Bold.woff2',
       weight: '600',
       style: 'bold',
     },
     {
-      path: '../public/static/font/GTWalsheimPro-Medium.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/static/font/GTWalsheimPro-UltraLight.woff2',
+      path: '../public/static/fonts/GTWalsheimPro-UltraLight.woff2',
       weight: '200',
       style: 'normal',
     },
