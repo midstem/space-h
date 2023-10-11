367042022edc69c986d47b0dcd7b9793fff4887e
Anatolii Olshevskyi <anatolkruv@gmail.com>
2023-10-11 21:50:07 +0300

    20 update env file

--- api/db/index.ts
+++ api/db/index.ts
@@ -1,8 +1,8 @@
 import mysql from 'mysql2'
 
 export const pool = mysql.createPool({
-  host: 'mysqldb',
-  user: 'space',
-  password: 'secret',
-  database: 'spaceh_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
