c09b498c82a553262947bf523cf4be4133fd797b
Anatolii Olshevskyi <anatolkruv@gmail.com>
2024-01-19 15:07:13 +0200

    drizzle add seed

--- api/src/schema.ts
+++
@@ -1,7 +0,0 @@
-import { mysqlTable, int, varchar, boolean } from 'drizzle-orm/mysql-core'
-
-export const todos = mysqlTable('todos', {
-  id: int('id').primaryKey().autoincrement(),
-  title: varchar('title', { length: 255 }),
-  completed: boolean('completed').default(false),
-})
